689d5c325246ddc8a9b91f99f1530110eca88d3b
Massimo Zambelli <mass.zambelli@gmail.com>
2018-10-27 19:51:06 +0200

    WIP

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+console.log(process.env.NODE_ENV);
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// POST /aircall/calls
+app.post('/aircall/calls', (req, res) => {
+    if (req.body.event === 'call.created') {
+        // Do something with this event
+        console.log('data', req.body.data);
+    }
+    else {
+        console.log('Event non-handled:', req.body.event);
+    }
+
+res.sendStatus(200);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
